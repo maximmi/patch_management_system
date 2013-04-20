f0279c887efe7d4e8360eb6217105f723ce79f8c
Igor Rodionov <goruha@gmail.com>
2013-04-20 11:39:43 +0700

    add deploy configs

---
+++ capistrano/instances/dpatchms.apertalab.no-ip.org.rb
@@ -0,0 +1,11 @@
+$app = "seo.apertalab.no-ip.org";
+$host = "apertalab.no-ip.org";
+$user = "www-data";
+$deploy_path = "/var/www/seo.apertalab.no-ip.org";
+$drush_path = "/bin/drush";
+
+$db_host = "localhost"
+$db_user = "root"
+$db_pass = "sibay@123"
+$db_name = "seo_content"
+$db_backup_path = $deploy_path + "/resources/backupdb/"
